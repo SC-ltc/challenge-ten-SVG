a67957daa4bee04ad983b51ad7010afb0260f0e6
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-09-05 15:12:21 -0700

    added triangle.js and a triangle test that passes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Module Ten Challenge - SVG Logo Maker
+
+
+## Resources used
+https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
+
+https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/text-anchor
---
+++ tests/triangle.test.js
@@ -0,0 +1,14 @@
+const Triangle = require('../lib/triangle.js');
+
+describe('Triangle', () => {
+    it('should return a triangle with shape color, text, and text color based on the user choice', () => {
+        const triangle = new Triangle('blue', 'ABC', 'white');
+        expect(triangle.render()).toEqual(`
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="50,0 100,100 0,100 fill="blue" />
+        <text x="100" y="100" font-size="50" fill="white" text-anchor="middle">ABC</text>
+        </svg>
+        `);
+    })
+})
+
